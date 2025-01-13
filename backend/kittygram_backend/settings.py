a5a3b9887f28fccdcbbabe7bd41b1d7e88e5c948
Савва Морозов <morsavvamorozov@yandex.ru>
2025-01-13 12:45:19 +0300

    add remarks

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,14 @@
 import os
+
+from dotenv import load_dotenv
+
 from pathlib import Path
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'default_secret_key')
 
 DEBUG = os.getenv('DEBUG')
 
